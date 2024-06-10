379484d611370bdf0d5e8b12b8a3f18525109079
Eder Fiuza <paulofiuza24@gmail.com>
2024-06-10 17:58:41 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the first value: ");
+
+            float v1 = float.Parse(Console.ReadLine()); 
+            
+
+            Console.WriteLine("Second value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+
+            float result = v1 + v2;
+            Console.WriteLine($"Result of the operation: {result}"); 
+
+        }
+    }
+}
